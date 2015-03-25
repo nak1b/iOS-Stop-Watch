aad1f12fb060d18466b0dc65b94e0907f6db3761
Nakib Momin <nbmomin14@gmail.com>
2015-03-25 15:24:04 -0400

    Added function for timer

--- stopWatch/ViewController.swift
+++ stopWatch/ViewController.swift
@@ -10,14 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func result(){
+        count++
+        print("\(count) \n")
     }
 
 
